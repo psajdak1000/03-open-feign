c8fbdfc4c4f4f55eae7bd8af59a08fde5a5d563b
Psajdak <psajdak1000@gmail.com>
2026-03-07 14:30:34 +0100

    open-feign communication itunes http

--- src/main/java/com/psajdak/modul3springboothttp/Modul3springbootHttpApplication.java
+++ src/main/java/com/psajdak/modul3springboothttp/Modul3springbootHttpApplication.java
@@ -1,15 +1,27 @@
 package com.psajdak.modul3springboothttp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 @EnableFeignClients
 public class Modul3springbootHttpApplication {
 
+    @Autowired
+    ShawnMendesProxy shawnMendesClient;
+
     public static void main(String[] args) {
         SpringApplication.run(Modul3springbootHttpApplication.class, args);
+    }
+
+    @EventListener(ApplicationStartedEvent.class)
+    public void makeRequestToShawnMendesEndpoint() {
+        String response = shawnMendesClient.makeSearchRequest("sabaton",1);
+        System.out.println("Response from Shawn Mendes API: " + response);
     }
 
 }
--- src/main/java/com/psajdak/modul3springboothttp/ShawnMendesProxy.java
+++ src/main/java/com/psajdak/modul3springboothttp/ShawnMendesProxy.java
@@ -2,11 +2,14 @@ package com.psajdak.modul3springboothttp;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("shawn-mendes-client")
+@FeignClient(value = "shawnmendes-client", url = "https://itunes.apple.com")
 public interface ShawnMendesProxy {
-    //GET https://api.shawnmendes.com/search
+    //GET https://itunes.apple.com/search?term=radiohead&limit=1
     @GetMapping("/search")
-    public String search();
+    public String makeSearchRequest(
+            @RequestParam("term") String term,
+            @RequestParam("limit") Integer limit
+    );
 }
