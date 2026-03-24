b09b374a70c03968cda503570118ba3926b32886
Psajdak <psajdak1000@gmail.com>
2026-03-24 23:24:17 +0100

    restTemplate vs openFeign

---
+++ src/main/java/com/psajdak/modul3springboothttp/RestTemplateExample2.java
@@ -0,0 +1,28 @@
+package com.psajdak.modul3springboothttp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+public class RestTemplateExample2 {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    public String makeShawnMendesRequest(String term, Integer limit) {
+
+        String uri = "https://itunes.apple.com/search?term="+ term +"&limit=" + limit;
+
+        ResponseEntity<String> exchange = restTemplate.exchange(
+                uri,
+                HttpMethod.GET,
+                null,
+                String.class
+
+
+        );
+        return exchange.getBody();
+    }
+}
