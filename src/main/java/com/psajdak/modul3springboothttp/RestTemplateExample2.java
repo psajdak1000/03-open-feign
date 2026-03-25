cd8e1569f4d7048adc5ba87cc9cb5620a58f6f8c
Psajdak <psajdak1000@gmail.com>
2026-03-25 22:49:00 +0100

    restTemplate vs openFeign

--- src/main/java/com/psajdak/modul3springboothttp/RestTemplateExample2.java
+++ src/main/java/com/psajdak/modul3springboothttp/RestTemplateExample2.java
@@ -3,9 +3,10 @@ package com.psajdak.modul3springboothttp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+@Component
 public class RestTemplateExample2 {
 
     @Autowired
